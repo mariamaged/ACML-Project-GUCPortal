b74021fc46acb1c0836a266905f2ea408573306a
Maria Maged <mariamaged988@gmail.com>
2020-12-16 07:14:53 +0200

    Models: Add the department attribute to CourseSchema.

--- Models/CourseModel.js
+++ Models/CourseModel.js
@@ -1,8 +1,10 @@
+const { ObjectID } = require('mongodb');
 const mongoose = require('mongoose');
 
 const CourseSchema = mongoose.Schema({
     id: {type: Number},
     name: {type: String},
+    department: {type: ObjectID, ref: 'Department'},
     slots_needed: {type: Number},
     slots_covered: {type: Number}
 },
