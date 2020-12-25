ec130329f5ec08ac3dfe02fc1c1b86814bf2b23e
maya <47913925+mayaeraky@users.noreply.github.com>
2020-12-25 23:59:46 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 const express = require('express')
 const app = express();
 const HRroutes= require('./Routes/HRroute');
-const instructorRoutes= require('./Routes/instructorRoutes');
+const instructorRoutes= require('./Routes/instructorRoutes.js');
 app.use(express.json());
-
+var bodyParser = require('body-parser');
+app.use(bodyParser.json());
 const UserAuthorizationRoutes = require('./Routes/UserAuthorization.js');
-
+app.listen(3000);
 
 app.use('/HR',HRroutes);
 app.use('/Instructor',instructorRoutes);
