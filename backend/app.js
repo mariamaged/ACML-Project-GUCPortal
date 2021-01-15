3f6b214ccbaba5ac329adcb48cd8b7a8a764801e
Monica George <monicageorge99@gmail.com>
2021-01-16 00:05:59 +0200

    m

--- backend/app.js
+++ backend/app.js
@@ -11,6 +11,13 @@ const app = express();
 app.use(express.json());
 
 app.use(cors())
+app.use((req,res,next)=>{
+    res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE")
+    res.header("Access-Control-Allow-Origin","*");
+    res.header("Access-Control-Allow-Headers","Origin, X-Requested-With, Content-Type, Accept,auth-token");
+    res.header("Access-Control-Expose-Headers","auth-token");
+    next();
+})
 // app.use().
 app.use('/HOD', HODRoutes);
 app.use('/Instructor', InstructorRoutes);
